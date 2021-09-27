120cddbf176b01df4338f0500bafa47270c352bc
ale5000 <15793015+ale5000-git@users.noreply.github.com>
2021-09-27 22:46:39 +0200

    Update scan.sh

--- scan.sh
+++ scan.sh
@@ -8,15 +8,13 @@ scan_file()  # $1 => File path to scan
 {
   if [[ "$1" =~ ${ext_regexp} ]]; then
     echo "Currently scanning: $1"
-    "${shellcheck_path}" --color=always "$1"
+    "${shellcheck_path}/shellcheck" --color=always "$1"
   fi
 }
 
 scan_folder()  # $1 => Folder path to scan
 {
-  if test -f "$1"; then
-    scan_file "$1"
-  elif test -d "$1"; then
+  if test -d "$1"; then
     for entry in "$1"/*; do
       if test -d "${entry}"; then
         scan_folder "${entry}"
@@ -24,6 +22,8 @@ scan_folder()  # $1 => Folder path to scan
         scan_file "${entry}"
       fi
     done
+  elif test -f "$1"; then
+    scan_file "$1"
   fi
 }
 
