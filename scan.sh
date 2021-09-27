98ab8643984137c906f2b217d13a16e2ef81fa6d
ale5000 <15793015+ale5000-git@users.noreply.github.com>
2021-09-27 21:48:20 +0200

    Create scan.sh

---
+++ scan.sh
@@ -0,0 +1,27 @@
+#!/usr/bin/env bash
+
+ext_regexp='\.(sh|bash|dash|ksh)$'
+
+scan_file()  # $1 => File path to scan
+{
+  if [[ "$1" =~ ${filename_regexp} ]]; then
+    shellcheck "$1"
+  fi
+}
+
+scan_folder()  # $1 => Folder path to scan
+{
+  if test -f "$1"; then
+    scan_file "$1"
+  elif test -d "$1"; then
+    for entry in "$1"/*; do
+      if test -d "${entry}"; then
+        scan_folder "${entry}"
+      else
+        scan_file "${entry}"
+      fi
+    done
+  fi
+}
+
+scan_folder .
