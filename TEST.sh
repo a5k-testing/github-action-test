1e90ee12b3953e8c94e306dd2d8aa310f500ffe5
ale5000 <15793015+ale5000-git@users.noreply.github.com>
2021-10-09 18:58:08 +0200

    Update TEST.sh

--- TEST.sh
+++ TEST.sh
@@ -1,3 +1,12 @@
-#!
+#!/bin/sh
+# @file libexample
+# @brief A library that solves some common problems.
+# @description
+#     The project solves lots of problems:
+#      * a
+#      * b
+#      * c
+#      * etc
+
 ...
 ...
